31fa3fb5f37c022173a9cdc09546869d645896f7
Elm1144 <f.schmaehl@gmx.de>
2022-06-27 20:01:16 +0200

    input player name addd

--- tictactoe.py
+++ tictactoe.py
@@ -1,1 +1,10 @@
 # This will be my first project for codecademy
+
+
+print("!!!!!!!!!!!!!!!!!!!!!!!!")
+print("Welcome to Tic Tac Toe!")
+print("!!!!!!!!!!!!!!!!!!!!!!!!")
+playersName = input("Please type in your name ")
+print("Welcome " + playersName)
+
+
